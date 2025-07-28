a07562b6fe15341c9ab865bc154121c1b8d3d763
Maiss Wanous <maisswanous.gmail.com>
2025-07-28 15:26:12 +0300

    home

--- src/assets/components/NavBar/Navbar.jsx
+++ src/assets/components/NavBar/Navbar.jsx
@@ -7,15 +7,18 @@ const Navbar = () => (
   <nav className="navbar">
     <div className="navbar-container">
       <div className="logo">postly</div>
-      <ul className="nav-links">
-        
-        <li><a href="/Home">home</a></li>
-        
-        <li><a href="/Profile">profile</a></li>
-        <li><a href="/about">about</a></li>
-      </ul>
-      <div className="btn-container">
-        <a href="/SignOut" className="btn-active">SIGN OUT</a>
+      <div className='d-flex align-items-center'>
+
+        <ul className="nav-links">
+
+          <li><a href="/Home">home</a></li>
+
+          <li><a href="/Profile">profile</a></li>
+          <li><a href="/about">about</a></li>
+        </ul>
+        <div className="btn-container">
+          <a href="/SignOut" className="btn-active">SIGN OUT</a>
+        </div>
       </div>
     </div>
   </nav>
